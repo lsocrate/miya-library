7cc5d1286e47678de65c3f1087e5af135d03aa66
Luiz Sócrate <luiz@socrate.com.br>
2021-06-11 01:27:59 +0200

    Card count for splash

--- src/UI/Decklist.elm
+++ src/UI/Decklist.elm
@@ -5,6 +5,7 @@ import Clan exposing (Clan(..))
 import Deck exposing (Deck)
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Tuple
 import UI.Icon
 
 
@@ -27,9 +28,11 @@ view model =
 
         { attachments, stronghold, role, conflictCharacters, conflictEvents, dynastyCharacters, dynastyEvents, provinces, holdings } =
             deck
+
+        influence =
+            influenceData stronghold.clan attachments conflictCharacters conflictEvents
     in
-    div
-        [ class "decklist" ]
+    div [ class "decklist" ]
         [ div [ class "decklist-stronghold" ]
             [ img
                 [ src <| "/assets/card-" ++ stronghold.id ++ ".webp"
@@ -37,7 +40,7 @@ view model =
                 ]
                 []
             ]
-        , div [ class "decklist-header" ]
+        , section [ class "decklist-header" ]
             [ h1 [ class "decklist-title" ] [ text <| Maybe.withDefault "Unnamed" name ]
             , p [ class "decklist-byline" ] [ text <| "By " ++ author ]
             , h2 [ class "decklist-synth" ]
@@ -46,10 +49,10 @@ view model =
                 , text <| Maybe.withDefault "" <| Maybe.map .title role
                 ]
             , p []
-                [ strong [] [ text "Influence:" ]
-                , text <| String.fromInt <| sumInfluence stronghold.clan attachments + sumInfluence stronghold.clan conflictCharacters + sumInfluence stronghold.clan conflictEvents
-                , text " of "
-                , text <|
+                ([ strong [] [ text "Influence:" ]
+                 , text <| String.fromInt <| List.foldl (\( _, ( _, clanTotal ) ) total -> total + clanTotal) 0 influence
+                 , text " of "
+                 , text <|
                     String.fromInt <|
                         case role of
                             Just { traits } ->
@@ -61,11 +64,14 @@ view model =
 
                             _ ->
                                 stronghold.influenceValue
-                ]
-            , ul [ class "decklist-provincelist" ] <| provinceEntry provinces
+                 , text " - "
+                 ]
+                    ++ List.map (\( clan, ( cardCount, _ ) ) -> span [] [ text <| String.fromInt cardCount, text " ", UI.Icon.small <| UI.Icon.clan clan ]) influence
+                )
+            , ul [ class "decklist-provincelist" ] <| List.map provinceEntries provinces
             ]
-        , div [ class "decklist-dynasty_deck" ]
-            (div []
+        , section [ class "decklist-dynasty_deck" ]
+            (h3 []
                 [ text "Dynasty Deck ("
                 , text <| String.fromInt <| sumCards dynastyCharacters + sumCards holdings + sumCards dynastyEvents
                 , text ")"
@@ -74,8 +80,8 @@ view model =
                 ++ cardBlock "Events" dynastyEvents
                 ++ cardBlock "Holdings" holdings
             )
-        , div [ class "decklist-conflict_deck" ]
-            (div []
+        , section [ class "decklist-conflict_deck" ]
+            (h3 []
                 [ text "Conflict Deck ("
                 , text <| String.fromInt <| sumCards attachments + sumCards conflictCharacters + sumCards conflictEvents
                 , text ")"
@@ -87,23 +93,20 @@ view model =
         ]
 
 
-provinceEntry : List Card.ProvinceProps -> List (Html msg)
-provinceEntry =
-    List.map
-        (\province ->
-            li [ class "decklist-province" ]
-                (text province.title
-                    :: (case province.element of
-                            Card.Single el ->
-                                [ UI.Icon.small <| UI.Icon.element el ]
+provinceEntries : Card.ProvinceProps -> Html msg
+provinceEntries province =
+    li [ class "decklist-province" ]
+        ([ text province.title, text " " ]
+            ++ (case province.element of
+                    Card.Single el ->
+                        [ UI.Icon.small <| UI.Icon.element el ]
 
-                            Card.Double el1 el2 ->
-                                [ UI.Icon.small <| UI.Icon.element el1, UI.Icon.small <| UI.Icon.element el2 ]
+                    Card.Double el1 el2 ->
+                        [ UI.Icon.small <| UI.Icon.element el1, UI.Icon.small <| UI.Icon.element el2 ]
 
-                            Card.Tomoe ->
-                                [ UI.Icon.small UI.Icon.Fiverings ]
-                       )
-                )
+                    Card.Tomoe ->
+                        [ UI.Icon.small UI.Icon.Fiverings ]
+               )
         )
 
 
@@ -115,11 +118,11 @@ cardBlock title cards =
                 Nothing
 
             else
-                Just (li [] [ text (String.fromInt n ++ "x " ++ card.title) ])
+                Just (li [ class "decklist-cardrow" ] [ text (String.fromInt n ++ "x " ++ card.title) ])
     in
     if sumCards cards > 0 then
-        [ div [] [ text <| title ++ " (" ++ String.fromInt (sumCards cards) ++ ")" ]
-        , ul [] <| List.filterMap cardRow cards
+        [ h4 [] [ text <| title ++ " (" ++ String.fromInt (sumCards cards) ++ ")" ]
+        , ul [ class "decklist-cardlist" ] <| List.filterMap cardRow cards
         ]
 
     else
@@ -131,60 +134,72 @@ sumCards =
     List.sum << List.map Tuple.second
 
 
-sumInfluence : Clan -> List (DecklistEntry { t | clan : Clan, influenceCost : Maybe Int }) -> Int
-sumInfluence deckClan =
+influenceData : Clan -> List (DecklistEntry Card.AttachmentProps) -> List (DecklistEntry Card.ConflictCharacterProps) -> List (DecklistEntry Card.ConflictEventProps) -> List ( Clan, ( Int, Int ) )
+influenceData deckClan attachments characters events =
     let
-        totalInfluence ( card, n ) =
-            if card.clan == deckClan then
-                0
+        addTuples ( xa, xb ) ( ya, yb ) =
+            ( xa + ya, xb + yb )
 
-            else
-                Maybe.map ((*) n) card.influenceCost |> Maybe.withDefault 0
+        init =
+            { crab = ( 0, 0 ), crane = ( 0, 0 ), dragon = ( 0, 0 ), lion = ( 0, 0 ), phoenix = ( 0, 0 ), scorpion = ( 0, 0 ), unicorn = ( 0, 0 ) }
+
+        influenceStatsByClan ( card, n ) grouped =
+            case ( card.clan, card.influenceCost ) of
+                ( _, Nothing ) ->
+                    grouped
+
+                ( Neutral, _ ) ->
+                    grouped
+
+                ( Shadowlands, _ ) ->
+                    grouped
+
+                ( Crab, Just influence ) ->
+                    { grouped | crab = Tuple.mapBoth ((+) n) ((+) (n * influence)) grouped.crab }
+
+                ( Crane, Just influence ) ->
+                    { grouped | crane = Tuple.mapBoth ((+) n) ((+) (n * influence)) grouped.crane }
+
+                ( Dragon, Just influence ) ->
+                    { grouped | dragon = Tuple.mapBoth ((+) n) ((+) (n * influence)) grouped.dragon }
+
+                ( Lion, Just influence ) ->
+                    { grouped | lion = Tuple.mapBoth ((+) n) ((+) (n * influence)) grouped.lion }
+
+                ( Phoenix, Just influence ) ->
+                    { grouped | phoenix = Tuple.mapBoth ((+) n) ((+) (n * influence)) grouped.phoenix }
+
+                ( Scorpion, Just influence ) ->
+                    { grouped | scorpion = Tuple.mapBoth ((+) n) ((+) (n * influence)) grouped.scorpion }
+
+                ( Unicorn, Just influence ) ->
+                    { grouped | unicorn = Tuple.mapBoth ((+) n) ((+) (n * influence)) grouped.unicorn }
     in
-    List.sum << List.map totalInfluence
-
-
-partition :
-    List (DecklistEntry Card.Card)
-    ->
-        { role : Maybe Card.RoleProps
-        , provinces : List Card.ProvinceProps
-        , conflictAttachments : List (DecklistEntry Card.AttachmentProps)
-        , conflictEvents : List (DecklistEntry Card.ConflictEventProps)
-        , conflictCharacters : List (DecklistEntry Card.ConflictCharacterProps)
-        , dynastyCharacters : List (DecklistEntry Card.DynastyCharacterProps)
-        , dynastyEvents : List (DecklistEntry Card.DynastyEventProps)
-        , dynastyHoldings : List (DecklistEntry Card.HoldingProps)
-        }
-partition =
-    let
-        intoCategories ( card, n ) categories =
-            case card of
-                Card.RoleType (Card.Role props) ->
-                    { categories | role = Just props }
-
-                Card.AttachmentType (Card.Attachment props) ->
-                    { categories | conflictAttachments = ( props, n ) :: categories.conflictAttachments }
-
-                Card.CharacterType (Card.ConflictCharacter props) ->
-                    { categories | conflictCharacters = ( props, n ) :: categories.conflictCharacters }
-
-                Card.CharacterType (Card.DynastyCharacter props) ->
-                    { categories | dynastyCharacters = ( props, n ) :: categories.dynastyCharacters }
-
-                Card.EventType (Card.ConflictEvent props) ->
-                    { categories | conflictEvents = ( props, n ) :: categories.conflictEvents }
-
-                Card.EventType (Card.DynastyEvent props) ->
-                    { categories | dynastyEvents = ( props, n ) :: categories.dynastyEvents }
-
-                Card.HoldingType (Card.Holding props) ->
-                    { categories | dynastyHoldings = ( props, n ) :: categories.dynastyHoldings }
-
-                Card.ProvinceType (Card.Province props) ->
-                    { categories | provinces = props :: categories.provinces }
-
-                _ ->
-                    categories
-    in
-    List.foldl intoCategories { role = Nothing, provinces = [], conflictAttachments = [], conflictEvents = [], conflictCharacters = [], dynastyCharacters = [], dynastyEvents = [], dynastyHoldings = [] }
+    [ List.foldl influenceStatsByClan init attachments
+    , List.foldl influenceStatsByClan init characters
+    , List.foldl influenceStatsByClan init events
+    ]
+        |> List.foldl
+            (\a b ->
+                { b
+                    | crab = addTuples a.crab b.crab
+                    , crane = addTuples a.crane b.crane
+                    , dragon = addTuples a.dragon b.dragon
+                    , lion = addTuples a.lion b.lion
+                    , phoenix = addTuples a.phoenix b.phoenix
+                    , scorpion = addTuples a.scorpion b.scorpion
+                    , unicorn = addTuples a.unicorn b.unicorn
+                }
+            )
+            init
+        |> (\b ->
+                [ ( Crab, b.crab )
+                , ( Crane, b.crane )
+                , ( Dragon, b.dragon )
+                , ( Lion, b.lion )
+                , ( Phoenix, b.phoenix )
+                , ( Scorpion, b.scorpion )
+                , ( Unicorn, b.unicorn )
+                ]
+           )
+        |> List.filter (\( clan, ( cardCount, _ ) ) -> clan /= deckClan && cardCount > 0)
